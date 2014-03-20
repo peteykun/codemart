ea61bfdb6125f7c1e940f2285cb0444b367eac88
Soham Pal <pt@live.in>
2014-03-20 21:45:15 +0530

    Allow false value for Program.solved

--- app/models/program.rb
+++ app/models/program.rb
@@ -2,5 +2,13 @@ class Program < ActiveRecord::Base
   belongs_to :user
   has_many :runs
 
-  validates_presence_of :name, :statement, :output, :input, :solved, :reward
+  validates_presence_of :name, :statement, :output, :input, :reward
+  validates_inclusion_of :solved, in: [true, false]
+
+  after_initialize :default_values
+
+  private
+    def default_values
+      self.solved ||= false
+    end
 end
